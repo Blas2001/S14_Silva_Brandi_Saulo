d41b0a31cc98a1954fb9fd53e2ae972193942427
Félix Armenta A <62727466+FelixH316@users.noreply.github.com>
2022-01-24 01:02:38 -0600

    Update T2E1_test_list.py

--- Tarea_02/T2E1_test_list.py
+++ Tarea_02/T2E1_test_list.py
@@ -1,3 +1,23 @@
+"""
+Date:       2021-11-17
+Authors:    Julio Cesar Rodriguez Castillo
+File:       
+Brief:      
+Score:      80 (15)
+Version:    1.1.1
+Fixes:      - Falto el encabezado (docstring)
+            
+            - PEP8 recomienda añadir un espacio en blanco después del
+                carácter de almohadilla '#' de los comentarios
+            
+            - Falta la condición de if __name__ == "__main__":
+            
+            - PEP8 recomienda añadir espacios en blanco alrededor de
+                los operadores, excepto en los paramétros
+            
+            - PEP8 recomienda añadir un espacio en blanco después del
+                carácter de coma ','
+"""
 """Crear una lista de n elementos de texto (strings) predefinidos, después realizar los
 siguientes pasos (comentar el número y nombre del paso) e imprimir la lista en
 cada paso: """
