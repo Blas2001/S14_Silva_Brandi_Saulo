42a35c1eb385cc9c525af9b70574cccd30d8f25c
Félix Armenta A <62727466+FelixH316@users.noreply.github.com>
2022-01-23 22:48:32 -0600

    Update T1E3_birthday.py

--- Tarea_01/T1E3_birthday.py
+++ Tarea_01/T1E3_birthday.py
@@ -1,7 +1,21 @@
-#Saulo Blas Silva Brandi
-#Ingresar tu fecha de cumpleaños como enteros
-#en variables por separado e imprimir junto al string
-#Year: xxxx - Month: xx - Day: xx
+"""
+Date:     2022-01-07
+Authors:  Saulo Blas Silva Brandi
+File:     T1E3_birthday.py
+Brief:    Este programa solicita año, mes y día de cumpleaños 
+          e implime el string 'Year:xxxx - Month:xx - Day: xx'
+          con sus respectivos resultados
+Score:    80
+Version:  1.1.1
+Fixes:    - Falta la condición de __main__
+          
+          - PEP8 recomienda no dejar espacio en blanco después de 
+                abrir '(' o antes de cerrar paretesis ')'
+             
+          - PEP8 recomienda añadir un espacio en blanco después del
+                carácter de coma ','    
+"""
+
 Year = int(input("Año de nacimiento : "))
 Month = int(input("Mes de nacimiento en nùmero : "))
 Day = int(input("Dìa de nacimiento : "))
