2d86f3f3a4bbed76ccb7e2dc17ea8f4e2c35433e
Félix Armenta A <62727466+FelixH316@users.noreply.github.com>
2022-01-23 22:43:37 -0600

    Update T1E2_even_odd.py

--- Tarea_01/T1E2_even_odd.py
+++ Tarea_01/T1E2_even_odd.py
@@ -1,5 +1,17 @@
-#Saulo Blas Silva Brandi
-#Ingresa un nùmero y valida si es par o impar
+"""
+Date:       2022-01-07
+Authors:    Saulo Blas Silva Brandi
+File:       T1E2_even_odd.py
+Brief:      Ingresar un número y validar si es par o impar.
+Score:      90
+Versión:    1.1.1
+Fixes:      - Falta la condición de __main__
+            
+            - PEP8 recomienda añadir espacios en blanco alrededor de
+                los operadores
+"""
+
+# SERIA BUENO AGREGAR UNA INDICACIÓN CUANDO EL RESULTADO ES CERO
 Num = int(input("Escribe un nùmero : "))
 if Num %2 == 0:
     print("Numero par")
